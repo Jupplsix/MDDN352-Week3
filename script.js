738731dcdeb1609b465a464f30b12aaf666cfd6b
Jupplsix <37137000+Jupplsix@users.noreply.github.com>
2018-04-12 21:03:01 +1200

    Update HTML, CSS and Javascript

--- script.js
+++ script.js
@@ -3,19 +3,20 @@
 // Get Required Elements from HTML
 
 var page = document.querySelector("#fullscreenCover");
+
 var tiltElement = document.querySelector("#tilt");
 
-var displayHeight;
 
+// Setup Required Variables
+
+var displayHeight;
 
 
 // Resize Page to Fit Screen
 
 function resizePageLoad() {
-	var height = window.innerHeight;
-	displayHeight = height;
-	height = height + "px";
-	displayHeight = height;
+	displayHeight = window.innerHeight;
+	page.style.height = displayHeight + "px";
 }
 
 
@@ -23,11 +24,9 @@ window.addEventListener('load', resizePageLoad);
 
 window.addEventListener('deviceorientation', function(event){
 
-	var height = event.beta;
+	var YAngle = event.beta;
+	tiltAdjust = map(YAngle, 45, 135, 0, displayHeight);
 
-	heightDisplay = map(height, 45, 135, 0, displayHeight);
-
-	heightDisplay = heightDisplay + "px";
-	tiltElement.style.height = heightDisplay;
+	tiltElement.style.height = tiltAdjust + "px";
 
 });
