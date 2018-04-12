2650e3e91e2be9444d1f95e3574930bb0a6e7836
Jupplsix <37137000+Jupplsix@users.noreply.github.com>
2018-04-12 20:47:31 +1200

    Update Javascript

--- script.js
+++ script.js
@@ -3,11 +3,9 @@
 // Get Required Elements from HTML
 
 var page = document.querySelector("#fullscreenCover");
-
 var tiltElement = document.querySelector("#tilt");
 
-var pos = document.querySelector("#pos");
-
+var displayHeight;
 
 
 
@@ -15,30 +13,21 @@ var pos = document.querySelector("#pos");
 
 function resizePageLoad() {
 	var height = window.innerHeight;
-	var heightpos = height/2;
-
-
+	displayHeight = height;
 	height = height + "px";
-	heightpos = heightpos + "px";
-
-	page.style.height = height;
-	pos.style.marginTop = heightpos;
+	displayHeight = height;
 }
 
-function geo(x, y) {
-	pos.textContent = (x + " " + y);
-}
 
 window.addEventListener('load', resizePageLoad);
 
 window.addEventListener('deviceorientation', function(event){
 
 	var height = event.beta;
-	height = height + "px";
+
+	heightDisplay = map(height, 45, 135, 0, displayHeight);
+
+	heightDisplay = heightDisplay + "px";
 	tiltElement.style.height = height;
 
-});
-
-navigator.geolocation.getCurrentPosition(function(position) {
-  geo(position.coords.latitude, position.coords.longitude);
 });
