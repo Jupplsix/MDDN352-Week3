91e7435712e5cd92ae8e8376e71a6a92af01a9f3
Jupplsix <37137000+Jupplsix@users.noreply.github.com>
2018-04-13 19:51:23 +1200

    Update JS

--- script.js
+++ script.js
@@ -1,26 +1,25 @@
 // Get Required Elements from HTML
 
 var page = document.querySelector("#fullscreenCover");
-
 var tiltElement = document.querySelector("#tiltProgress");
 
 // Resize Page to Fit Screen
 
 function resizePageLoad() {
+
 	var height = window.innerHeight;
-
-
 	height = height + "px";
-
 	page.style.height = height;
 }
+
 
 window.addEventListener('load', resizePageLoad);
 
 window.addEventListener('deviceorientation', function(event){
 
 	var height = event.beta;
-	height = height + "px";
-	tiltElement.style.height = height;
+	tiltAdjust = map(height, 0, 180, 0, displayHeight);
+	tiltAdjust = tiltAdjust + "px";
+	tiltElement.style.height = tiltAdjust;
 
 });
