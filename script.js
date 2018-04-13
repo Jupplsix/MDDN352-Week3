c70f24d3879e64a783acfb8c6be69e1b1950017d
Jupplsix <37137000+Jupplsix@users.noreply.github.com>
2018-04-13 20:31:12 +1200

    Update

--- script.js
+++ script.js
@@ -3,29 +3,24 @@
 var page = document.querySelector("#fullscreenCover");
 var tiltElement = document.querySelector("#tiltProgress");
 
+// Setup Required Variables
+
+var displayHeight;
+
+
 // Resize Page to Fit Screen
 
 function resizePageLoad() {
-
-	var height = window.innerHeight;
-	height = height + "px";
-	page.style.height = height;
+	displayHeight = window.innerHeight;
+	page.style.height = displayHeight + "px";
 }
 
 
 window.addEventListener('load', resizePageLoad);
 
+
 window.addEventListener('deviceorientation', function(event){
 
-	var height = event.beta;
-
-	// tiltAdjust = map(height, 0, 180, 0, displayHeight);
-	tiltAdjust = (height-0)/(180-0) * ((window.innerHeight)-0) + 0;
-
-	tiltAdjust = tiltAdjust + "px";
-
-	console.log(height);
-	console.log(tiltAdjust);
-	tiltElement.style.height = tiltAdjust;
-
+	var YAngle = event.beta;
+	console.log(YAngle);
 });
